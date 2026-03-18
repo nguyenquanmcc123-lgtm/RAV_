006057cde3e9e4008e7229b0b8d73278e3538af1
nguyenquanmcc123-lgtm <nguyenquanmcc123@gmail.com>
2026-03-18 18:04:58 +0700

    Update periodic.js

--- periodic.js
+++ periodic.js
@@ -1,22 +1,16 @@
+window.onload = () => {
+
 const table = document.getElementById("periodic-table");
-const info = document.getElementById("element-info");
 
-elements.forEach(e=>{
+elements.forEach(e => {
 
-const div=document.createElement("div");
+const div = document.createElement("div");
 
-div.className="element";
-div.innerHTML=`<b>${e.symbol}</b><br>${e.number}`;
-
-div.onclick=()=>{
-info.innerHTML=`
-<h2>${e.name}</h2>
-<p>Số nguyên tử: ${e.number}</p>
-<p>Khối lượng: ${e.mass}</p>
-<a href="https://vi.wikipedia.org/wiki/${e.name}" target="_blank">Wikipedia</a>
-`;
-};
+div.className = "element";
+div.innerText = e.symbol;
 
 table.appendChild(div);
 
 });
+
+};
